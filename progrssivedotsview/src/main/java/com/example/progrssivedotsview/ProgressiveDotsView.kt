f7c4f2d2f6a40a86649198744a3de60c5b860b26
Anwesh43 <anweshthecool0@gmail.com>
2021-04-06 00:58:56 +0530

    adding PDNode class

--- progrssivedotsview/src/main/java/com/example/progrssivedotsview/ProgressiveDotsView.kt
+++ progrssivedotsview/src/main/java/com/example/progrssivedotsview/ProgressiveDotsView.kt
@@ -120,4 +120,45 @@ class ProgressiveDotsView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PDNode(var i : Int, val state : State = State()) {
+
+        private var next : PDNode? = null
+        private var prev : PDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PDNode {
+            var curr : PDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
