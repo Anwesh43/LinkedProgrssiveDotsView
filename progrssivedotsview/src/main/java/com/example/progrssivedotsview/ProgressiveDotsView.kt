e7cf29a358bb92a12643340f1fabd77c4c5329c7
Anwesh43 <anweshthecool0@gmail.com>
2021-04-06 00:45:23 +0530

    adding Canvas extension methods

--- progrssivedotsview/src/main/java/com/example/progrssivedotsview/ProgressiveDotsView.kt
+++ progrssivedotsview/src/main/java/com/example/progrssivedotsview/ProgressiveDotsView.kt
@@ -30,3 +30,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawProgressiveDots(w : Float, h : Float, scale : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / wFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val r : Float = size / (3 * balls + 1)
+    save()
+    translate(w / 2, h / 2)
+    paint.style = Paint.Style.STROKE
+    drawRect(RectF(-size * sf1, -2 * r * sf1, size * sf1, 2 * r * sf1), paint)
+    for (j in 1..balls) {
+        save()
+        translate(-size + 2 * (j + 1) * r, -2 * r)
+        drawCircle(0f, 0f, r * sf.divideScale(j, parts), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawPDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawProgressiveDots(w, h, scale, paint)
+}
